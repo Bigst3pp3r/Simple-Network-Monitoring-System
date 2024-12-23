e2ad2a3327bcda110b8e5d42f734320add2abf8e
Milton Karani <77004395+Bigst3pp3r@users.noreply.github.com>
2024-12-23 12:18:12 +0300

    Log file setup and rotation

--- src/logging_setup.py
+++ src/logging_setup.py
@@ -1,1 +1,13 @@
 # Log file setup and rotation
+# logger_setup.py
+import logging
+from logging.handlers import RotatingFileHandler
+
+def setup_logging(log_file, max_bytes, backup_count):
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    logging.basicConfig(
+        handlers=[handler],
+        level=logging.INFO,
+        format="%(asctime)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
